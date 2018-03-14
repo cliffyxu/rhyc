72895874f4d9f50191aecd18208115612be966b5
cliffyxu <33970918+cliffyxu@users.noreply.github.com>
2018-03-13 20:20:23 -0400

    Add files via upload

--- alarm_parser.py
+++ alarm_parser.py
@@ -1,3 +1,78 @@
+def remove_tag(om):
+    om_new = om
+    s_i = om.find("<")
+    #size = om.size()
+    while(s_i != -1):
+        e_i = om.find(">", s_i)
+        if(e_i != -1):
+            #om_new = om[e_i+1:]
+            om_new_1 = ''
+            if(s_i > 0):
+                om_new_1 = om[0:s_i]
+            om_new_2 = om[e_i+1:]
+            om_new_3 = om_new_1 + om_new_2 
+            #print s_i, e_i
+            #print "om_new_1: (" + om_new_1 + ")"
+            #print "om_new_2: (" + om_new_2 + ")"
+            #print "Om_new_3: (" + om_new_3 + ")"
+            om_new = om_new_3
+            om = om_new
+            s_i = om.find("<")
+        else:
+            print("missing closing >")
+            print om
+            break
+    #print "om_new: "+om_new
+    
+    return om_new.rstrip()
+    
+def remove_token(fmt):
+    fmt_new = fmt
+    s_i = fmt.find("%")
+    while(s_i != -1):
+        e_i_1 = fmt.find(":", s_i)
+        e_i_2 = fmt.find(" ", s_i)
+        e_i = e_i_1
+        if(e_i == -1):
+            e_i = e_i_2
+        elif(e_i_2 != -1):
+            if(e_i_2 < e_i):
+                e_i = e_i_2
+        if(e_i != -1):
+            fmt_new_1 = ''
+            if(s_i > 0):
+                fmt_new_1 = fmt[0:s_i]
+            fmt_new_2 = fmt[e_i:]
+            fmt_new = fmt_new_1 + fmt_new_2
+            fmt = fmt_new
+            s_i = fmt.find("%")
+        else:
+            print("token not ending with space or : could be the end")
+            print fmt
+            if(s_i > 0):
+                fmt_new = fmt[0:s_i-1]
+                fmt = fmt_new
+            break
+    #print "fmt_new: " + fmt_new
+    return fmt_new.rstrip()
+
+text_mismatch = 0
+    
+def compare(om, fmt):
+    global text_mismatch
+    result = "No"
+    om_new = remove_tag(om)
+    fmt_new = remove_token(fmt)
+    if( om_new == fmt_new ):
+        result = "yes"
+    else:
+        print("om and fmt do not match...")
+        print( "om:<" + om_new + ">")
+        print("fmt:<" + fmt_new + ">")
+        text_mismatch += 1
+    return result
+
+
 count = 0
 bad_act_alarm_count = 0
 alarm_dict={}
@@ -39,9 +114,11 @@ match = 0
 for row in csv.reader(alarm_old):
     if new_count == 0:
         row.insert(2, "Alarm From Act File")
+        row.insert(3, "Match")
         print row[14]
         print row
     else:
+        match_act = "no"
         alarm_equate_orig = row[13]
         alarm_equate = alarm_equate_orig.strip(" ")
         #print
@@ -50,6 +127,7 @@ for row in csv.reader(alarm_old):
         #print row
         act_value = alarm_dict.get(alarm_equate)      
         if act_value != None:
+            match_act = compare(row[1], act_value)
             match = match +1 
             #print act_value
             #row.insert(2, act_value)
@@ -57,21 +135,22 @@ for row in csv.reader(alarm_old):
             #print row
         else:
             act_value=""
+            match_act = "none"
             print
             print "<<"+alarm_equate+">>"+" does not have FMT"
             
         row.insert(2, act_value)
+        row.insert(3, match_act )
     writer.writerow(row)
     new_count = new_count+1
 print
 print "ACT has alarms: " , count, " including bad alarms: ", bad_act_alarm_count
 print "Master list has alarms: ", new_count - 1
-print "Matches between ACT and Master list: ", match, "mismatches: ", new_count - match -1 
-
+print "Matches of EQ between ACT and Master list: ", match, "mismatches: ", new_count - match -1 
+print "Text mismatch between ACT and Master List: ", text_mismatch
   
 alarm_old.close()
 alarm_new.close()
-
 
 #num = 1
 #for keys,values in alarm_dict.items():
--- renhan.py
+++ renhan.py
@@ -9,7 +9,6 @@ while True:
     a = line.find("EQ")
     b = line.find(" ", a + 3)
     eq = line[a + 3:b]
-    #dictionary.update({eq : None})
     fmt = ""
     c = line.find("FMT")
     d = line.find('"', c + 5) 
@@ -17,6 +16,29 @@ while True:
     dictionary[eq] = fmt
     number_lines += 1
         
-print number_lines
-print dictionary
+#print number_lines
+#print dictionary
 taipei.close()
+
+import csv
+with open(r"C:\Data\xuc\Python\tcl_alarms.csv", "rb") as csvfile:
+    csvreader = csv.reader(csvfile)
+    #for row in csvreader:
+        #print row
+    csvfile1 = open(r"C:\Data\xuc\Python\tcl_alarms1.csv", "wb+")
+    csvwriter = csv.writer(csvfile1)
+    i = 1
+    for row in csvreader:
+        format = None
+        if i > 1:
+            equate = row[13]
+            if dictionary.has_key(equate):
+                format = dictionary[equate]
+            else:
+                print "Missing Equate in ACT file:", equate
+        else:
+            format = "ACT Fromat"           
+        i += 1
+        row.insert(2, format)
+        csvwriter.writerow(row)
+    csvfile1.close()
